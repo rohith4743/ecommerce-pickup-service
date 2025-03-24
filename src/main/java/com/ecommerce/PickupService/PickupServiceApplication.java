b4788ac844767a7d29462a6311ccf2f5d5182783
rohith4743 <rohithkankipati4743@gmail.com>
2025-03-23 22:48:45 -0400

    first commit

---
+++ src/main/java/com/ecommerce/PickupService/PickupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.PickupService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/PickupService/PickupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.PickupService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
